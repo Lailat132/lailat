7187631bf21bfa170a22b9909c70e9b08ddc76ab
USER <USER@192.168.219.100>
2022-07-28 23:21:02 +0900

    ㅎㅎ..

--- Lailat/src/gugu.java
+++ Lailat/src/gugu.java
@@ -3,11 +3,12 @@ public class gugu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("±¸±¸´Ü ");
+		System.out.println("êµ¬êµ¬ë‹¨");
 		
 		for(int i=2; i<=9; i++) {
+			System.out.println(i + " ë‹¨");
 			for(int j=1; j<=9; j++)
-				System.out.println(i + "*" + j);
+				System.out.println(i + "*" + j + "=" + (i * j));
 		}
 	}
 
